dc5179cd4567bc36b567ed0e557844fbd27b937d
Rubenvdabeele <rubenvdabeele@gmail.com>
2022-11-14 20:31:07 +0100

    Array creation excersises.

---
+++ src/main/java/com/rvda/arrays/excercises/ArrayExcercises.java
@@ -0,0 +1,25 @@
+package com.rvda.arrays.excercises;
+
+public class ArrayExcercises {
+    // Maak de volgende arrays
+    // [1,2,3,4] Integer
+    // ["hey", "wat", "handig"] String
+    // [[1,2], [1,2,3,9],[1,0]] Integer
+
+    // Maak van het derde element 13
+    Integer[] updateMe = {2,8,9,0};
+
+    // Print het eerste, het 3e en het laatste element van bovenstaande array.
+    
+
+    // Print het eerste/enigste element van onderstaande arrays.
+    // Zijn je wat je verwacht?
+
+    String[] emptyStringArray = new String[1];
+    boolean[] emptyBooleanArray = new boolean[1];
+    int[] emptyPrimitiveIntArray = new int[1];
+    Integer[] emptyIntegerArray = new Integer[1];
+
+
+
+}
